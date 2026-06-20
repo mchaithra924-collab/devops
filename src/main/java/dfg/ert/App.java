15e32bf29d25b09d005b4bcc5f819de23a7a7029
m chaithra <mchaithra924@gmail.com>
2026-06-20 09:55:17 +0530

    first commit

---
+++ src/main/java/dfg/ert/App.java
@@ -0,0 +1,13 @@
+package dfg.ert;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/dfg/ert/aapp.java
@@ -0,0 +1,7 @@
+package dfg.ert;
+
+public class aapp {
+	public static void main(String[] args) {
+		System.out.println("hello");
+	}
+}
